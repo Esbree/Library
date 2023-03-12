13a53aeb9783158a609669fa3ab99f705e0ebd91
Esbree <noelva@gmx.de>
2023-03-12 10:41:05 +0100

    store user input as book inside array

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let myLibrary = []
+
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -8,3 +10,25 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.read}`
 }
+
+const titleInput = document.getElementById('title')
+const authorInput = document.getElementById('author')
+const pagesInput = document.getElementById('pages')
+const readInput = document.getElementById('read')
+const submitButton = document.getElementById('btn-submit')
+
+function addBookToLibrary(title, author, pages, read) {
+  title = titleInput.value
+  author = authorInput.value
+  pages = pagesInput.value
+  read = readInput.checked
+
+  const book = new Book(title, author, pages, read)
+
+  myLibrary.push(book)
+  console.log(myLibrary);
+}
+
+submitButton.addEventListener('click', function() {
+  addBookToLibrary()
+})
