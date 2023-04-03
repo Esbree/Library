695900e1d97e68ca53bba284c2560e95c787fa80
Esbree <noelva@gmx.de>
2023-04-03 22:06:45 +0200

    commit

--- app.js
+++ app.js
@@ -5,20 +5,18 @@ function Book() {
 }
 
 
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const pages = document.querySelector('#pages')
+const read = document.querySelector('#read')
+
 function addBookToLibrary() {
   
 }
 
 const addBtn = document.querySelector('.btn-add')
 const modalContainer = document.querySelector('.modal-container')
-const modalCloseBtn = document.querySelector('.modal-confirm-btn')
 
 addBtn.addEventListener('click', function() {
   modalContainer.classList.add('show')
 })
-
-modalCloseBtn.addEventListener('click', function() {
-  modalContainer.classList.remove('show')
-})
-
-// create addBookToLibrary function -> click on OK button
