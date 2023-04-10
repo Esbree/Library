b842daebd94e8677f1edacc919a1d5dbb65c9d98
Esbree <noelva@gmx.de>
2023-04-10 21:23:38 +0200

    creates a book object

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 let myLibrary = []
 
-function Book() {
-  
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
-
-const title = document.querySelector('#title')
-const author = document.querySelector('#author')
-const pages = document.querySelector('#pages')
-const read = document.querySelector('#read')
 
 function addBookToLibrary() {
-  
+  let title = document.querySelector('#title').value
+  let author = document.querySelector('#author').value
+  let pages = document.querySelector('#pages').value
+  let read = document.querySelector('#read').checked
+  let newBook = new Book(title, author, pages, read)
+  console.log(newBook)
 }
 
+// add button - pop up modal
 const addBtn = document.querySelector('.btn-add')
 const modalContainer = document.querySelector('.modal-container')
 
 addBtn.addEventListener('click', function() {
   modalContainer.classList.add('show')
+})
+
+// submit button
+const submitbtn = document.querySelector('#submit')
+
+submitbtn.addEventListener('click', function() {
+  event.preventDefault()
+  addBookToLibrary()
 })
